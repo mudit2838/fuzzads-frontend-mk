2670927d017ae6e1cf3f4020f6d9df3fe8136cd9
mudit chauhan <80970437+mudit2838@users.noreply.github.com>
2026-02-25 22:53:29 +0530

    Update Sidebar.jsx

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast'; // <-- 1. Import toast
-import API_BASE_URL from "@/utils/api";
+
 
 import { 
   ShoppingBag, 
