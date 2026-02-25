f7399f2314ca4f8056336f037b73f499bd4ecb2b
mudit chauhan <80970437+mudit2838@users.noreply.github.com>
2026-02-25 19:16:45 +0530

    Update PublicLayout.jsx

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../components/common/Navbar';
 import Footer from '../components/common/Footer';
 import FloatingButtons from '../components/common/FloatingButtons';
-
+import API_BASE_URL from "@/utils/api";
 const PublicLayout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-white">
